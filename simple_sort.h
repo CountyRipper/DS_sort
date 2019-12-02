0ef5c0427091888a3fab29756b514de9b1cd7204
‘Maverick <1097249217@qq.com>
2019-12-02 10:15:07 +0800

    header

---
+++ simple_sort.h
@@ -0,0 +1,10 @@
+
+template<class Type>
+void BubbleSort(Type elem[],int n);
+
+template<class Type>
+void InsertSort(Type elem[],int n);
+
+template<class Type>
+void SelectSort(Type elem[],int n);
+
